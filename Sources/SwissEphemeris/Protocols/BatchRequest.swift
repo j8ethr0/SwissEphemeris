baefa445d9684a077d97dca42520524d031daa76
Fergal J Monahan <fergal@Fergals-MacBook-Pro.local>
2025-03-06 18:09:42 +0700

    Fix batch + planet issues

--- Sources/SwissEphemeris/Protocols/BatchRequest.swift
+++ Sources/SwissEphemeris/Protocols/BatchRequest.swift
@@ -12,28 +12,3 @@ public protocol BatchRequest {
     ///   - interval: The interval between each data point.
     func fetch(start: Date, end: Date, interval: TimeInterval) async -> [Coordinate<Body>]
 }
-
-public extension BatchRequest {
-     func fetch(start: Date, end: Date, interval: TimeInterval) async -> [Coordinate<Body>] {
-        let startJd = start.julianDate()
-        let endJd = end.julianDate()
-        let intervalJd = interval / (24 * 60 * 60)
-
-        return await withTaskGroup(of: Coordinate<Body>?.self, returning: [Coordinate<Body>].self) { group in
-            var results: [Coordinate<Body>] = []
-            for julianDay in stride(from: startJd, through: endJd, by: intervalJd) { //removed strideable
-                group.addTask {
-                    let date = Date(julianDay: julianDay) //removed strideable, uses extension
-                    return try? Coordinate(body: self.body, date: date)
-                }
-            }
-
-            for await result in group {
-                if let result = result {
-                    results.append(result)
-                }
-            }
-            return results
-        }
-    }
-}
--- Sources/SwissEphemeris/Utility/PlanetsRequest.swift
+++ Sources/SwissEphemeris/Utility/PlanetsRequest.swift
@@ -11,32 +11,4 @@ public struct PlanetsRequest: BatchRequest {
     public init(body: Planet) {
         self.body = body
     }
-
-    public func fetch(start: Date, end: Date, interval: TimeInterval) async -> [Coordinate<Planet>] { // Added 'public'
-        let startJd = start.julianDate()
-        let endJd = end.julianDate()
-        let intervalJd = interval / (24 * 60 * 60) // Convert seconds to Julian Day fraction
-
-        return await withTaskGroup(of: Coordinate<Planet>?.self, returning: [Coordinate<Planet>].self) { group in
-            var results: [Coordinate<Planet>] = []
-
-            // Capture 'body' and 'intervalJd' directly, NOT self
-            let localBody = self.body
-
-            for julianDay in stride(from: startJd, through: endJd, by: intervalJd) { //removed strideable
-                group.addTask {
-                    let date = Date(julianDay: julianDay) //removed strideable, uses extension
-                    // Use localBody here
-                    return try? Coordinate(body: localBody, date: date)
-                }
-            }
-
-            for await result in group {
-                if let result = result {
-                    results.append(result)
-                }
-            }
-            return results
-        }
-    }
 }
